4df584f04458448ef7f67eb29a46434283e7fe98
Blanca Abril Gonzalez <32962533+blancaabril@users.noreply.github.com>
2018-10-21 21:11:12 +0200

    WS1T1-Actualizada

---
+++ T1/WS 1 T1/index.js
@@ -0,0 +1,7 @@
+var primera = 1;
+var segunda = 2;
+var suma = primera + segunda;
+
+document.write("El valor de la primera variable es " + primera + "<br>");
+document.write("El valor de la segunda variable es " + segunda + "<br>");
+document.write("El valor de la suma es " + suma);
