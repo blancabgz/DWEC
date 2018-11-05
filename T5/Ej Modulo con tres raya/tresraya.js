41ee7e02ae8fa9648f9fd49baf49c94921e38a52
Blanca Abril Gonzalez <32962533+blancaabril@users.noreply.github.com>
2018-11-05 13:06:04 +0100

    comentarios

--- T5/Ej Modulo con tres raya/tresraya.js
+++ T5/Ej Modulo con tres raya/tresraya.js
@@ -11,7 +11,7 @@ export class tresRaya{
 	constructor(){
 		this.a=false; // jugador 1
 		this.b=false; // jugador 2
-		this.hueco=false; // 
+		this.hueco=false; // Hueco para el modulo para saber si esta lleno o vacio, si esta a true, esta lleno
 		this.tablero = []; // un array con las posiciones del tablero 
 		this.finalizado = false; // el juego ha terminado, cuando esta a falso, el juego sigue funcionando, cuando esta a true, el juego ha terminado porque algun jugador ha ganado
 		this.turno1 = true; // variable para manejar los turnos, cuando esta en true le toca al jugador a, cuando es falso, juega el b
@@ -42,22 +42,22 @@ export class tresRaya{
 	}
 
 
-	seleccionarCasilla(fila,columna){
+	seleccionarCasilla(fila,columna){ // seleccionar la casilla y la columna para marcar donde el usuario quiere colocar la ficha
 		if(this.finalizado == false){
-			if(this.tablero[fila][columna] == 0){
+			if(this.tablero[fila][columna] == 0){ // mientras el hueco no este ocupado por a o por b 
 				this.hueco = false;
-				if(this.turno1 == true){
+				if(this.turno1 == true){ // turno verdadero juega a
 					this.tablero[fila][columna] = "a";
 					this.turno1 = false;
 				}else{
-					this.tablero[fila][columna] = "b";
+					this.tablero[fila][columna] = "b"; // turno falso juega b
 					this.turno1 = true;
 				}
 				
 			}else{
-				this.hueco = true;
+				this.hueco = true; 
 			}
-			this.comprobarSolucion();
+			this.comprobarSolucion(); // comprueba si la jugada ha sido ganadora 
 		}else{
 			console.log("La partida ha acabado");
 		}
@@ -72,7 +72,7 @@ export class tresRaya{
             	}else if(this.tablero[0][0] == "b"){
             		this.b=true;
             	}
-            	this.finalizado = true;  
+            	this.finalizado = true;  // la jugada ha terminado
         }
         if(this.tablero[1][0] == this.tablero[1][1] && this.tablero[1][0] == this.tablero[1][2]
             && (this.tablero[1][0] == "a" || this.tablero[1][0] == "b")){
@@ -81,7 +81,7 @@ export class tresRaya{
             }else if(this.tablero[1][0] == "b"){
             	this.b=true;
             }
-            this.finalizado = true;
+            this.finalizado = true; // la jugada ha terminado
         }
         if(this.tablero[2][0] == this.tablero[2][1] && this.tablero[2][0] == this.tablero[2][2]
             && (this.tablero[2][0] == "a" || this.tablero[2][0] == "b")){
@@ -90,7 +90,7 @@ export class tresRaya{
             }else if(this.tablero[2][0] == "b"){
             	this.b=true;
            	}
-            this.finalizado = true;
+            this.finalizado = true; // la jugada ha terminado
         }
         //Comprobaciones verticales
         if(this.tablero[0][0] == this.tablero[1][0] && this.tablero[0][0] == this.tablero[2][0]
@@ -100,7 +100,7 @@ export class tresRaya{
             }else if(this.tablero[0][0] == "b"){
             	this.b=true;
             }
-            this.finalizado = true;
+            this.finalizado = true; // la jugada ha terminado
         }
         if(this.tablero[0][1] == this.tablero[1][1] && this.tablero[0][1] ==this.tablero[2][1]
             && (this.tablero[0][1] == "a" || this.tablero[0][1] == "b")){
@@ -109,7 +109,7 @@ export class tresRaya{
             }else if(this.tablero[0][1] == "b"){
             	this.b=true;
             }
-            this.finalizado = true;
+            this.finalizado = true; // la jugada ha terminado
         }
         if(this.tablero[0][2] == this.tablero[1][2] && this.tablero[0][2] == this.tablero[2][2]
             && (this.tablero[0][2] == "a" || this.tablero[0][2] == "b")){
@@ -128,7 +128,7 @@ export class tresRaya{
             }else if(this.tablero[0][0] == "b"){
             	this.b=true;
             }
-            this.finalizado = true;
+            this.finalizado = true; // la jugada ha terminado
         }
         if(this.tablero[0][2] == this.tablero[1][1] && this.tablero[0][2] == this.tablero[2][0]
             && (this.tablero[0][2] == "a" || this.tablero[0][2] == "b")){
@@ -137,11 +137,11 @@ export class tresRaya{
             }else if(this.tablero[0][2] == "b"){
             	this.b=true;
             }
-            this.finalizado = true;
+            this.finalizado = true; // la jugada ha terminado
         }
     }
 }
 
-var traya = new tresRaya();
+var traya = new tresRaya(); // creacion del objeto
 
 
